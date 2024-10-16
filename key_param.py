f473c38ca5952d5d5faed2dcf4bedb0b9ce729c7
Leonardo Gamboa <leogamboa@hotmail.com>
2024-10-16 22:03:19 +0200

    Update key_param.py

--- key_param.py
+++ key_param.py
@@ -1,5 +1,5 @@
 # api_key openai
-openai_api_key = "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"
+openai_api_key = "sk-xxx-xxx-xxx"
 
 # Mongo Atlas DB
 MONGO_URI = "mongodb+srv://novagenio:Superleo01.@cluster0.brrnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -7,7 +7,7 @@ MONGO_dbName = "langchain_demo"
 MONGO_collectionName = "collection_of_text_blobs"
 
 # Qdrant
-qdrant_api_key ='HC1gC83H8s4EPc8zPuRnT3M08s9gz9ADfNPSBQDPoqENERatlDpTXw'
+qdrant_api_key ='xxxxxxxxxxx3M08s9gz9ADfNPSBQDPoqENERatlDpTXw'
 QDRANT_URL ='https://1965c967-7ba5-4608-8f25-d4a4e8f5baec.europe-west3-0.gcp.cloud.qdrant.io:6333'
 QDRANT_collection_name = "my_documents"
 
