4a20d9a211f8d4558572fd1b89bccc4c4c746ad7
Leonardo Gamboa <leogamboa@hotmail.com>
2024-10-16 21:57:55 +0200

    Add files via upload

---
+++ key_param.py
@@ -0,0 +1,25 @@
+# api_key openai
+openai_api_key = "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"
+
+# Mongo Atlas DB
+MONGO_URI = "mongodb+srv://novagenio:Superleo01.@cluster0.brrnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_dbName = "langchain_demo"
+MONGO_collectionName = "collection_of_text_blobs"
+
+# Qdrant
+qdrant_api_key ='HC1gC83H8s4EPc8zPuRnT3M08s9gz9ADfNPSBQDPoqENERatlDpTXw'
+QDRANT_URL ='https://1965c967-7ba5-4608-8f25-d4a4e8f5baec.europe-west3-0.gcp.cloud.qdrant.io:6333'
+QDRANT_collection_name = "my_documents"
+
+# Local folder to 
+chroma_db ='chroma_db'
+faiss_db ='faiss_db'
+
+# pompt
+prompt = "Respond only with information provided in the context provided. If you don't have the answer, say sorry, I don't know the answer."
+
+# path pdf files
+path_pdf = 'pdf/eu_ai_act.pdf'
+
+# embedding model 
+model = "text-embedding-ada-002"
