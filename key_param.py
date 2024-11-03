9be03bf75c0b5e908cf70100751eabfed5b84034
Leonardo Gamboa <leogamboa@hotmail.com>
2024-11-03 14:38:03 +0100

    Update key_param.py

--- key_param.py
+++ key_param.py
@@ -1,22 +1,35 @@
 # api_key openai
-openai_api_key = "sk-xxx-xxx-xxx"
+openai_api_key = "xxx-xxx-xxx-xxx"
 
 # Mongo Atlas DB
-MONGO_URI = "mongodb+srv://novagenio:Superleo01.@cluster0.brrnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-MONGO_dbName = "langchain_demo"
-MONGO_collectionName = "collection_of_text_blobs"
+MONGO_URI = "mongodb+srv://novagenio:xxxxxxxxxxxxxx@cluster0.brrnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_dbName = "langchain_mongo"
+MONGO_collectionName = "collection_mongo"
 
 # Qdrant
-qdrant_api_key ='xxxxxxxxxxx3M08s9gz9ADfNPSBQDPoqENERatlDpTXw'
-QDRANT_URL ='https://1965c967-7ba5-4608-8f25-d4a4e8f5baec.europe-west3-0.gcp.cloud.qdrant.io:6333'
-QDRANT_collection_name = "my_documents"
+qdrant_api_key ='xxx-xxx-xxx-xxx-xxx'
+QDRANT_URL ='https://xxxxxxx-7ba5-4608-8f25-d4a4e8f5baec.europe-west3-0.gcp.cloud.qdrant.io'
+QDRANT_collection_name = "collection_qdrant"
+QDRANT_MODEL_NAME="Qdrant/bm25"
+
+# elastics
+elastic_api_key='xxx-xxx-xxx-xxxx'
+es_cloud_id='My_deployment:xxx-xxx-xxx-xxxx'
+user_elastics='elastic'
+pwd_elastic='xxx-xx-xx-xxx-xxx-xxx-xxx'
+elastic_url='https://my-deployment-nnnnnn.es.northeurope.azure.elastic-cloud.com:9200'
+elastic_index_name="langchain_index"
+
+# pinecone
+pinecone_api_key='xxxx-xxxx-xxxx-xxxx-xxxx'
+pinecone_index_name = "langchain-test-index"
 
 # Local folder to 
 chroma_db ='chroma_db'
 faiss_db ='faiss_db'
 
 # pompt
-prompt = "Respond only with information provided in the context provided. If you don't have the answer, say sorry, I don't know the answer."
+prompt = "Respond with information provided in the context provided. If you don't have the answer, say sorry, I don't know the answer."
 
 # path pdf files
 path_pdf = 'pdf/eu_ai_act.pdf'
